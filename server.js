cb7a8038dc3fe624c24a5aa8765e8d1fecc6ddd5
gareth-cheeseman <garethcheeseman@humbug.local>
2018-06-19 23:07:19 +0100

    Adds votes for comment

--- server.js
+++ server.js
@@ -341,8 +341,40 @@ function deleteComment(url, request) {
 
   return response;
 }
-function upvoteComment(id) {}
-function downvoteComment(id) {}
+function upvoteComment(url, request) {
+  const id = Number(url.split('/').filter(segment => segment)[1]);
+  const username = request.body && request.body.username;
+  let savedComment = database.comments[id];
+  const response = {};
+
+  if (savedComment && database.users[username]) {
+    savedComment = upvote(savedComment, username);
+
+    response.body = { comment: savedComment };
+    response.status = 200;
+  } else {
+    response.status = 400;
+  }
+
+  return response;
+}
+function downvoteComment(url, request) {
+  const id = Number(url.split('/').filter(segment => segment)[1]);
+  const username = request.body && request.body.username;
+  let savedComment = database.comments[id];
+  const response = {};
+
+  if (savedComment && database.users[username]) {
+    savedComment = downvote(savedComment, username);
+
+    response.body = { comment: savedComment };
+    response.status = 200;
+  } else {
+    response.status = 400;
+  }
+
+  return response;
+}
 
 // Write all code above this line.
 
